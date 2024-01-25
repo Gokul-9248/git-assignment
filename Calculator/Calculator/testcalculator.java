841c8faf51bf4f4274e79270cfeb00d92ce9842a
root <root@ip-172-31-226-17.us-east-2.compute.internal>
2024-01-25 20:17:23 +0000

    this is my 1st commit

---
+++ Calculator/Calculator/testcalculator.java
@@ -0,0 +1,30 @@
+package Calculator;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testcalculator {
+
+	@Test
+	public void calTest() {
+		assertEquals(31, calculator.add(11, 20));
+	}
+
+	@Test
+	public void calTestSub() {
+		assertEquals(30, calculator.sub(50, 20));
+	}
+
+	@Test
+	public void calcTestMul() {
+		assertEquals(30, calculator.mul(6, 5));
+	}
+
+	@Test
+	public void calcTestDiv() {
+		assertEquals(6, calculator.div(30, 5));
+	}
+
+}
